96a50adb9c0a125e2d0f080580e4c84930de93bd
Yunosuke <qmkns246@gmail.com>
2019-12-27 23:18:56 +0900

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by 堺雄之介 on 2019/12/27.
+//  Copyright © 2019 Yunosuke Sakai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
