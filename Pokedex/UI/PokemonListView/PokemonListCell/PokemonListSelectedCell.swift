c593e1038f11cc2aa3ef929dac73d63bb2040df9
Yunosuke <qmkns246@gmail.com>
2019-12-28 18:10:03 +0900

    選択中のポケモンの囲いをRoundedRectangleにしdrop shadowをつけた

--- Pokedex/UI/PokemonListView/PokemonListCell/PokemonListSelectedCell.swift
+++ Pokedex/UI/PokemonListView/PokemonListCell/PokemonListSelectedCell.swift
@@ -15,6 +15,8 @@ struct PokemonListSelectedCell: View {
         VStack {
             ZStack {
                 Color(red: 221/255, green: 91/255, blue: 68/255)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+                    .shadow(color: .gray, radius: 5, x: 0, y: 5)
                 
                 PokemonListNormalCell(pokemon: pokemon)
             }
